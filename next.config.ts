7e8495d9da44c6d947696752fd85ed7ab63e78b4
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-06-17 18:35:47 +0530

    setup: github app and webhook

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['b938-2a09-bac5-3e0f-11c3-00-1c5-77.ngrok-free.app']
 };
 
 export default nextConfig;
